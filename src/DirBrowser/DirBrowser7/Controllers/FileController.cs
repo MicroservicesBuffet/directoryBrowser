2da017e8b60c78087b9ddff06682cc11dea931f7
Andrei Ignat <ignatandrei@yahoo.com>
2023-02-28 23:38:48 +0200

    download and goto

--- src/DirBrowser/DirBrowser7/Controllers/FileController.cs
+++ src/DirBrowser/DirBrowser7/Controllers/FileController.cs
@@ -16,40 +16,63 @@ public class FileController : ControllerBase
         return folders;
     }
     [HttpGet("{*path}")]
-    public async Task<FolderToRead[]> GetFolderContent(string path,[FromServices] FolderToRead[] folders)
+    public async Task<FileResult> GetFileContent(string path, [FromServices] FolderToRead[] folders)
     {
         await Task.Delay(5000);
-        //todo: html decode
+        var f = GetFirstFolder(path, folders);
+        var str = path.Split("/", StringSplitOptions.RemoveEmptyEntries);
+        var pathFull = f.TransformFullPath;
+        for (int i = 1; i < str.Length - 1; i++)
+        {
+            var path1 = str[i];
+            pathFull = Path.Combine(pathFull, path1);
+            if (!Directory.Exists(pathFull))
+                throw new DirectoryNotFoundException($"{pathFull} must exists");
+
+        }
+        var file = Path.Combine(pathFull, str[str.Length - 1]);
+        return PhysicalFile(file, "application/octet-stream");
+    }
+    static FolderToRead GetFirstFolder(string path, FolderToRead[] folders)
+    {
         if (path.Contains("%2f"))
             path = path.Replace("%2f", "/");
         if (path.Contains("%2F"))
             path = path.Replace("%2F", "/");
 
-        var str = path.Split("/");
+        var str = path.Split("/", StringSplitOptions.RemoveEmptyEntries);
         if (str.Length == 0)
             throw new ArgumentException($"{nameof(path)} must have data");
-        
+
         var f = folders.FirstOrDefault(it =>
 
         string.Equals(it.Id, str[0], StringComparison.InvariantCultureIgnoreCase));
 
         if (f == null)
         {
-            var startF = string.Join("|",folders.Select(it => it.Id).ToArray());
+            var startF = string.Join("|", folders.Select(it => it.Id).ToArray());
             throw new ArgumentException($"{nameof(path)} = {path} must start with folders {startF}");
         }
-            var pathFull = f.TransformFullPath;
+        return f;
+    }
+    [HttpGet("{*path}")]
+    public async Task<FolderToRead[]> GetFolderContent(string path, [FromServices] FolderToRead[] folders)
+    {
+        await Task.Delay(5000);
+        var f = GetFirstFolder(path, folders);
+        var str = path.Split("/", StringSplitOptions.RemoveEmptyEntries);
+        var pathFull = f.TransformFullPath;
         for (int i = 1; i < str.Length; i++)
         {
-            var path1= str[i];
+            var path1 = str[i];
             pathFull = Path.Combine(pathFull, path1);
-            if(!Directory.Exists(pathFull) )
+            if (!Directory.Exists(pathFull))
                 throw new DirectoryNotFoundException($"{pathFull} must exists");
 
         }
         var di = new DirectoryInfo(pathFull);
-        var files=di.EnumerateFiles().ToArray().Select(it=>new FolderToRead(it)).ToArray();
-        var dirs= di.EnumerateDirectories().ToArray().Select(it => new FolderToRead(it)).ToArray();
+        var files = di.EnumerateFiles().ToArray().Select(it => new FolderToRead(it)).ToArray();
+        var dirs = di.EnumerateDirectories().ToArray().Select(it => new FolderToRead(it)).ToArray();
 
         List<FolderToRead> ret = new();
         if (dirs.Length > 0)
--- src/DirBrowser/dir_react_browser/src/justDisplayGUI/displayFoldersAndFiles.tsx
+++ src/DirBrowser/dir_react_browser/src/justDisplayGUI/displayFoldersAndFiles.tsx
@@ -1,4 +1,4 @@
-import { TableContainer, Table, TableCaption, Thead, Tr, Th, Tbody, Td, Tfoot, } from "@chakra-ui/react";
+import { TableContainer, Table, TableCaption, Thead, Tr, Th, Tbody, Td, Tfoot, Button, } from "@chakra-ui/react";
 import { FolderToRead } from "../genericFiles/FolderToRead";
 import {Link } from 'react-router-dom';
 type PropsDisplayFoldersAndFiles = {
@@ -13,18 +13,22 @@ export default function DisplayFoldersAndFiles(
     const files=allData.filter(it=>!it.isDirectory);
     const dirs=allData.filter(it=>it.isDirectory);
     const folderParentDisplay='/show/'+ (folderParent?folderParent+'/':'');
+    const downloadFile =(path:string)=>{
+      var url ='http://localhost:5288/api/v1.0/File/GetFileContent/';
+      window.alert(url+folderParent+'/'+path);
+    }
 
     return <>
     
     <TableContainer>
   <Table variant='striped'>
-    <TableCaption>this is data {allData?.length}</TableCaption>
+    <TableCaption>{(folderParent.length>0) && <> Content of {folderParent}</>}</TableCaption>
     <Thead>
       <Tr>
         <Th>Nr</Th>
         <Th>Name</Th>
-        <Th>IsFolder</Th>
-        <Th isNumeric>Operations</Th>
+        <Th>Type</Th>
+        <Th >Operations</Th>
       </Tr>
     </Thead>
     <Tbody>
@@ -32,8 +36,16 @@ export default function DisplayFoldersAndFiles(
         <Tr  key={it.id}>
         <Td>{index+1}</Td>
         <Td><Link to={folderParentDisplay + it.name}>{it.name}</Link> </Td>
-        <Td>{it.isDirectory?"YES":"NO" }</Td>
+        <Td>{it.isDirectory?"Folder":"File" }</Td>
         <Td>
+            {it.isDirectory &&
+            <Button colorScheme='teal'>
+              <Link to={folderParentDisplay + it.name}>GoTo</Link>
+              </Button>
+            }
+            {!it.isDirectory && 
+              <Button onClick={()=>downloadFile(it.name)} colorScheme='blue'>Download</Button>
+              }
             
         </Td>
       </Tr>
