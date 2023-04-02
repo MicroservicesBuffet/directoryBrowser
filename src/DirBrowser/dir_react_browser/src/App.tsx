9427132dea4d9cd2033544533feb23162fbe251f
Andrei Ignat <ignatandrei@yahoo.com>
2023-04-02 19:11:33 +0300

    added username

--- src/DirBrowser/dir_react_browser/src/App.tsx
+++ src/DirBrowser/dir_react_browser/src/App.tsx
@@ -5,64 +5,69 @@ import {
   VStack,
   theme,
   StackDivider,
-  Link
+  Button,
 } from "@chakra-ui/react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import DisplayFiles from "./DisplayFiles";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 // import { ColorModeSwitcher } from "./ColorModeSwitcher"
 
 export const App = () => {
 
-
+  const [userName,setUserName]=useState("  please wait");
+  const obtainUser=(): Promise<any>=>
+    fetch(''+process.env.REACT_APP_URL+'api/usefull/user/authorization', 
+    
+    {
+      method: 'GET',
+      credentials: 'include' 
+    }
+    )
+        .then(
+          (res) => {
+            if(res.status === 200 )
+              return res.json() ;
+            
+              return {name: 'Error'};
+          }
+      )
+  ;
+  
+    useEffect(() => {
+      obtainUser()
+      .then(it=> {
+        setUserName(it.name);
+      });
+  
+  }, []);
   const queryClient = new QueryClient();
-
   
 return (
+  <>
   <ChakraProvider theme={theme}>
-    <VStack
-  divider={<StackDivider borderColor='gray.200' />}
-  spacing={4}
-  align='stretch'
->
-  <Box textAlign="center"  h='40px' bg='white' fontSize="30px">
-    Directory browser and editor   &nbsp;
-    <Link
-            color="teal.500"
-            href="/help/"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Help
-          </Link>
-  </Box>
-  
+
+  <div>
+   <h1> Directory browser and editor   
+   <Button colorScheme='teal'>
+    <Link color="red"  to="/help/" target="_blank">
+    Help
+    </Link>
+    </Button>
+    <span style={{float: 'right'}} >{userName}</span>   
+</h1>&nbsp;
+    
+       
+          
+  </div>
   <Box>
     
     <QueryClientProvider client={queryClient}>
       <DisplayFiles  />
     </QueryClientProvider>
   </Box>
-</VStack>
-    {/* <Box textAlign="center" >
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8} >
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box> */}
+   
   </ChakraProvider>
+  </>
 )
   }
